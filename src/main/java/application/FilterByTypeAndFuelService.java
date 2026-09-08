ae64532a3479038095be969ae2aa3e7b2a88963e
Bernat Mestres <ber.mestres@gmail.com>
2026-09-08 12:55:37 +0200

    feat: FilterByTypeAndFuelService

---
+++ src/main/java/application/FilterByTypeAndFuelService.java
@@ -0,0 +1,29 @@
+package application;
+
+import domain.Car;
+import domain.CarRepository;
+import domain.FilterByTypeAndFuelQuery;
+import domain.FuelType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterByTypeAndFuelService implements FilterByTypeAndFuelQuery {
+
+    CarRepository cars;
+
+    public FilterByTypeAndFuelService(CarRepository cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public List<Car> filter(Class <? extends Car> carType, FuelType fuelType) {
+
+        return this.cars
+                .getAll()
+                .stream()
+                .filter(car -> carType.isInstance(car))
+                .filter(car -> car.getFuelType().equals(fuelType))
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/domain/FilterByTypeAndFuelQuery.java
+++ src/main/java/domain/FilterByTypeAndFuelQuery.java
@@ -1,4 +1,7 @@
 package domain;
 
+import java.util.List;
+
 public interface FilterByTypeAndFuelQuery {
+    List<Car> filter(Class <? extends Car> carType, FuelType fuelType);
 }
